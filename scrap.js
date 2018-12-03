6e370c485d53160ec747a95121a5a200be337f0e
Nielene Thomas <nielenethomas@pursuit.org>
2018-12-03 16:23:32 -0500

    showing on browser

---
+++ scrap.js
@@ -0,0 +1,14 @@
+const ai = () => {
+  let comp = Math.random();
+
+  switch (comp) {
+    case comp >= 0.0 && comp <= 0.33: r
+      break;
+    case comp >= 0.34 && comp <= 0.66: p
+      break;
+    case comp >= 0.67 && comp <= 1: s
+      break;
+    default:
+      null;
+  }
+}
